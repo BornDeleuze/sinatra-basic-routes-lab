ccdf80766fd81394d5b13982a558b270fe283d8e
Matt Beaudoin <ehxtwo@gmail.com>
2021-04-01 23:09:55 -0400

    commit tree 982b853d1d7336c5aebb2e3970bbf7eb230c8224

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Matt"
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is Inle"
+      end
+
+      get '/hometown' do
+        "My hometown is Philadelphia"
+      end
 end
